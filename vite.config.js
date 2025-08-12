6d360e4f5a190edb0bb0c6dd3127f7ced96e7387
Anish Kumar Raut <87709442+1anishraut@users.noreply.github.com>
2025-08-13 00:14:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './', // ✅ Use relative paths for assets
   server: {
     host: true, // <-- allow external access
     port: 5173, // optional: set a fixed port
